de7ad5beb4699f3060601c07867f06b3a3010c8d
Bria White <bria.awhite@gmail.com>
2020-10-04 15:14:48 +0000

    Automatically backed up by Learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,13 @@
 class Pirate
+  
+  @@all = []
+  
+  attr_accessor :name, :weight, :height 
+  
+  def initialize 
+    @name = name 
+    @weight = weight 
+    @height = height 
+  end 
+  
 end
