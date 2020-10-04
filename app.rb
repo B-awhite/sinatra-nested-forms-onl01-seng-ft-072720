a69a1089956b8037392888ec23cc1cf06b3292dc
Bria White <bria.awhite@gmail.com>
2020-10-04 15:47:33 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,6 +4,13 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+    get "/" do 
+      erb :"/instructions"
+    end 
+    
+    get "/new" do 
+      
+    end 
 
   end
 end
